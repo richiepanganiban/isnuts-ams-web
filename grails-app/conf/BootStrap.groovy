bad293f9a171e9cb859714038b5de4b5a3652bb6
Jonathan Tan <jonathan.tan@orangeandbronze.com>
2011-11-02 18:34:51 +0800

    added categories to bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
 import com.orangeandbronze.ams.MobileService
 import com.orangeandbronze.ams.MobileServiceType
+import com.orangeandbronze.ams.MobileServiceCategory
 import com.orangeandbronze.ams.KeywordItem
 import com.orangeandbronze.ams.KeywordItemType
 
@@ -8,23 +9,35 @@ class BootStrap {
     def init = { servletContext ->
 		def service, keyword
 		
+		def generalCategory = new MobileServiceCategory(name:'General').save(flush:true)
+		def callOffersCategory = new MobileServiceCategory(name:'Call Offers').save(flush:true)
+		def textOffersCategory = new MobileServiceCategory(name:'Text Offers').save(flush:true)
+		
 		// Balance Inquiry
 		service = new MobileService(published:true, serviceType:MobileServiceType.SMS,
 			title:'Balance Inquiry', description:'Inquire your balance.  Cost: Free', serviceNumber:'222').save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.LITERAL, literalValue:'BAL').save(flush:true)
+		generalCategory.addToServices(service)
+		generalCategory.save(flush:true)
 
 		// Pasaload
 		service = new MobileService(published:true, serviceType:MobileServiceType.SMS,
 			title:'Pasa Load', description:'Share load to family and friends.  Cost: P1 per transaction', serviceNumber:'2', appendMobileToServiceNumber:true).save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.USER_INPUT, label:'Amount').save(flush:true)
+		generalCategory.addToServices(service)
+		generalCategory.save(flush:true)
 
 		// Super Sakto Call
 		service = new MobileService(published:true, serviceType:MobileServiceType.CALL,
 			title:'Super Sakto Calls', description:'Call your friends on Globe and TM for only P0.15 per second!', serviceNumber:'232', appendMobileToServiceNumber:true).save(flush:true)
+		callOffersCategory.addToServices(service)
+		callOffersCategory.save(flush:true)
 
 		// Tawag 236
 		service = new MobileService(published:true, serviceType:MobileServiceType.CALL,
 			title:'TAWAG 236 ', description:'20 minutes of chika for only 20 pesos!', serviceNumber:'236', appendMobileToServiceNumber:true).save(flush:true)
+		callOffersCategory.addToServices(service)
+		callOffersCategory.save(flush:true)
 
 		// Duo
 		service = new MobileService(published:true, serviceType:MobileServiceType.SMS,
@@ -34,6 +47,10 @@ class BootStrap {
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.USER_INPUT, label:'Area').save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.SPACE).save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.LITERAL, literalValue:'450').save(flush:true)
+		callOffersCategory.addToServices(service)
+		callOffersCategory.save(flush:true)
+		textOffersCategory.addToServices(service)
+		textOffersCategory.save(flush:true)
 	
 		// Super Duo
 		service = new MobileService(published:true, serviceType:MobileServiceType.SMS,
@@ -43,6 +60,10 @@ class BootStrap {
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.USER_INPUT, label:'Area').save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.SPACE).save(flush:true)
 		keyword = new KeywordItem(mobileServiceInstance:service, itemType:KeywordItemType.LITERAL, literalValue:'599').save(flush:true)
+		callOffersCategory.addToServices(service)
+		callOffersCategory.save(flush:true)
+		textOffersCategory.addToServices(service)
+		textOffersCategory.save(flush:true)
     }
     def destroy = {
     }
